64adba21140ff7b1a5214db6d26dd9820c3cdedd
“Floreal <floreal.risso@univ-tlse3.fr>
2022-10-31 13:24:52 +0100

    Fix : the date isn't given by the culture.gouv.fr API, now it uses data.gouv.fr API.

--- lib/providers/server_api.dart
+++ lib/providers/server_api.dart
@@ -6,16 +6,31 @@ import 'dart:io';
 /// `ServerApi`is a static class for get the last version of the data.
 class ServerApi {
   static const String stableMetadataUrl =
+      "https://www.data.gouv.fr/api/1/datasets/5af120f2b595087cfabcde86/";
+
+  @Deprecated(
+      "Should not be use, it's for the parsing of the result from the data.culture.gouv.fr API.")
+  static const String stableMetadataUrlOld =
       "https://data.culture.gouv.fr/api/v2/catalog/datasets/base-franceterme-termes-scientifiques-et-techniques";
   static const String stableDataUrl =
       "https://www.data.gouv.fr/fr/datasets/r/4e1b0ebe-e40f-4ce2-beaa-ac5a60d20899";
 
-  static String _parseDate(dynamic json) {
+  @Deprecated(
+      "Should not be use, it's for the parsing of the result from the data.culture.gouv.fr API.")
+  // ignore: unused_element
+  static String _parseDateOld(dynamic json) {
     final String lastmodified = json['dataset']['metas']['default']['modified'];
     final String dataProcessed =
         json['dataset']['metas']['default']['data_processed'];
     final int compareResult = lastmodified.compareTo(dataProcessed);
     return compareResult >= 0 ? lastmodified : dataProcessed;
+  }
+
+  static String _parseDate(dynamic json) {
+    final String lastmodified = json["last_modified"];
+    final String lastupdate = json['last_update'];
+    final int compareResult = lastmodified.compareTo(lastupdate);
+    return compareResult >= 0 ? lastmodified : lastupdate;
   }
 
   /// Retrieve the date of the last update
